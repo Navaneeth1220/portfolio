3a7ee343d873bf98627f1d39e91fb14ee713f70e
Navaneeth S <nava8092@gmail.com>
2024-01-20 18:14:47 +0530

    homepage logo animation done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -12,8 +12,12 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}/>
-         <Route index element={<Home />} />
+      
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />}/>
+      </Route>
+        {/*<Route path="/" element={<Layout />}/>
+       <Route index element={<Home />} />
           {/* <Route path="about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portfolio />} />
--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,17 +1,58 @@
 import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 
 const Home = () => {
+
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [ 'a', 'v', 'a', 'n', 'e', 'e', 't', 'h'  ]
+  const jobArray = ['f','u','l','l',' ','s','t','a','c','k',' ','d','e','v','e','l','o','p','e','r']
+  const hiArray = ['H','i',',']
+  const imArray = ['I',"'",'m']
+
+  useEffect(() => {
+     setTimeout(() => {
+    return setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
 <div className="container home-page">
   <div className="text-zone">
-    <h1> Hi, <br /> I'm </h1>
-    <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
+    <h1> <AnimatedLetters
+              letterClass={letterClass}
+              strArray={hiArray}
+              idx={15}
+    /> <br /> <AnimatedLetters
+    letterClass={letterClass}
+    strArray={imArray}
+    idx={15}
+    />  
+<img src={LogoTitle} alt="JavaScript Developer Name, Web Developer Name"/>
+    <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
     />
+     <br/><AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={15}
+    /> </h1>
+     <h2>Front End Developer / Java SpringBoot Expert / QA/QE Automation / DJ</h2>
+     <Link to="/contact" className="flat-button">
+            CONTACT ME
+    </Link>
   </div>
+  <Logo />
 </div>
-
+  );
+  
 }
+export default Home
 
 /*import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
@@ -87,4 +128,4 @@ const Home = () => {
   )
 } */
 
-export default Home
+
